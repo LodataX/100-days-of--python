941ae83298865cd86abd847979d0b0627b0fb91e
LodataX <edimosharon4@gmail.com>
2025-07-23 10:36:43 +0100

    Create Day01_ BrandNameGeneratot.py
    Implemented a Python scripts that generates random brand names by combining words, This project is part of the 100 Days of code Boot-camp by Angela Yu and helps practice string manipulation and randomness in Python.

---
+++ Day01_ BrandNameGeneratot.py
@@ -0,0 +1,5 @@
+print("Welcome to Brand Name Generator!")
+city = input("What city di you grow up in?\n")
+pet= input("What is the name of your pet?\n")
+print("Your Band Name could be :" + city + " " + pet)
+      
